887acc02c0426e9a08b69b55eee7c32f2dc51555
kjerinic <katarinajerinic8@gmail.com>
2019-12-12 13:57:41 +0100

    pocetni kod

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const webPush = require('web-push');
-const Datastore = require('nedb');
+// require pakete koji nam trebaju: express i node-fetch
 
-const publicVapidKey = "BCKcmijEXML89kXg5Zt-aZAEPzOO-fFbA38L5pCUak8vA89MU6DYKTR26KHry359Vkowlln-l2KFwQbusLQLayQ";
-const privateVapidKey = "J0tcj43HVkp-TW3HWnbztZBHJ2FPCQo_M0KCl1cqdoQ";
+
+// api kljuc za vesti
 const guardianApiKey = "05572908-fc58-4061-a4e7-2f9cc2e990f1";
 
+// pokreni express aplikaciju
 
-const app = express();
-app.listen(3000, () => console.log("Server ceka zahteve."));
-app.use(express.static('public'));
-app.use(express.json({ limit: '2mb' }));
+// pokreni server na portu 3000
 
-const baza = new Datastore('baza.db');
-baza.loadDatabase();
-
-//push notifs
-webPush.setVapidDetails(
-    "mailto:kjtest@test.com",
-    publicVapidKey,
-    privateVapidKey
-);
 
 app.get('/drzava/:latlon', async (zah, odg) => {
     var apikey = '0b47275ff17b4922bc17fef296eede57';
     var api_url = 'https://api.opencagedata.com/geocode/v1/json';
 
+    // parsiraj parametre iz zahteva
     const latlon = zah.params.latlon.split(',');
     console.log(latlon);
-    const lat = latlon[0];
-    const lon = latlon[1];
+    const lat;
+    const lon;
 
     var request_url = api_url
         + '?'
@@ -39,8 +26,9 @@ app.get('/drzava/:latlon', async (zah, odg) => {
         + '&pretty=1'
         + '&no_annotations=1';
 
-    const fetchOdg = await fetch(request_url);
-    const jsonGeolok = await fetchOdg.json();
+    // daj odgovor
+    const fetchOdg;
+    const jsonGeolok;
     odg.json(jsonGeolok);
 });
 
@@ -48,86 +36,23 @@ app.get('/drzava/:latlon', async (zah, odg) => {
 app.get('/desetclanaka/:drzava', async (zah, odg) => {
     const drzava = zah.params.drzava;
 
-    const timestamp = Date.now();
-    baza.insert({ drzava: drzava, vreme: timestamp });
     console.log(drzava);
 
-    const urlD = `https://content.guardianapis.com/search?q=${drzava}&api-key=${guardianApiKey}`;
+    const guardianDrzavaURL = `https://content.guardianapis.com/search?q=${drzava}&api-key=${guardianApiKey}`;
 
-    const guardianOdgD = await fetch(urlD);
-    const jsonGuardD = await guardianOdgD.json();
+    const guardianDrzavaODG = await fetch(guardianDrzavaURL);
+    const jsonDrzava = await guardianDrzavaODG.json();
 
-    console.log(jsonGuardD.response.userTier);
+    // vracamo niz clanaka
 
-    //vracamo niz clanaka
-    odg.json(jsonGuardD.response.results);
 });
 
 app.get('/desetclanakasvet', async (zah, odg) => {
-    const urlS = `https://content.guardianapis.com/search?api-key=${guardianApiKey}`;
+    const guardianSvetURL = `https://content.guardianapis.com/search?api-key=${guardianApiKey}`;
 
-    const guardianOdgS = await fetch(urlS);
-    const jsonGuardS = await guardianOdgS.json();
-
-    console.log(jsonGuardS.response.userTier);
+    const guardianSvetODG = await fetch(guardianSvetURL);
+    const jsonSvet = await guardianSvetODG.json();
 
     //vracamo niz clanaka
-    odg.json(jsonGuardS.response.results);
-});
-
-app.get('/desetclanakasvet/:kriterijumPretrage', async (zah, odg) => {
-    var kat = zah.params.kriterijumPretrage;
-    const urlSU = `https://content.guardianapis.com/search?q=${kat}&api-key=${guardianApiKey}`;
-
-    const guardianOdgSU = await fetch(urlSU);
-    const jsonGuardSU = await guardianOdgSU.json();
-
-    console.log(jsonGuardSU.response.userTier);
-
-    //vracamo niz clanaka
-    odg.json(jsonGuardSU.response.results);
-});
-
-
-app.get('/desetclanakadrzava/:drzavaKategorija', async (zah, odg) => {
-    const drzavaKategorija = zah.params.drzavaKategorija.split(",");
-    const drzavaDU = drzavaKategorija[0];
-    const kategorijaDU = drzavaKategorija[1];
-    console.log(drzavaDU);
-    console.log(kategorijaDU);
-    const urlDU = `https://content.guardianapis.com/search?q=${drzavaDU}%20${kategorijaDU}&api-key=${guardianApiKey}`;
-
-    const guardianOdgDU = await fetch(urlDU);
-    const jsonGuardDU = await guardianOdgDU.json();
-
-    console.log(jsonGuardDU.response.userTier);
-
-    //vracamo niz clanaka
-    odg.json(jsonGuardDU.response.results);
-});
-
-
-//ruta za subscribe
-app.post("/subscribe", (zahtev, odgovor) => {
-    //uzimamo objekat subscription od klijenta
-    const subscription = zahtev.body;
-
-    //za vracenje odgovora
-    //vracamo status uspesno pravljenje
-    odgovor.status(201).json({});
-
-    const payload = JSON.stringify({
-        title: "Dobrodošli na Gardijanovu progresivnu veb aplikaciju!",
-        body: "telo poruke"
-    });
-
-    //saljemo ovaj objekat
-    webPush.sendNotification(subscription, payload).catch(greska => console.error(greska));
-});
-
-app.post("/komentar", (zahtev, odgovor) => {
-    const slikaKomentar = zahtev.body;
-    baza.insert(slikaKomentar);
-    odgovor.json(slikaKomentar);
 
 });
--- public/sw.js
+++ public/sw.js
@@ -2,48 +2,37 @@ const nazivKesMem = "prvaVerzija";
 
 const statSadrzaj = [
     '/',
-    'index.html',
-    'druga.html',
-    'manifest.json',
-    'css/style.css',
-    'css/materialize.min.css',
-    'js/materialize.min.js'
+    
+    'images/icons/icon-72x72.png',
+    'images/icons/icon-96x96.png',
+    'images/icons/icon-128x128.png',
+    'images/icons/icon-144x144.png',
+    'images/icons/icon-152x152.png',
+    'images/icons/icon-192x192.png',
+    'images/icons/icon-384x384.png',
+    'images/icons/icon-512x512.png'
 ];
-
 
 self.addEventListener('install', async e => {
     console.log("instaliran sw");
 
-    const kesMem = await caches.open(nazivKesMem);
-    await kesMem.addAll(statSadrzaj);
 
-    return self.skipWaiting();
+
 });
 
 self.addEventListener('activate', async e => {
     console.log("aktiviran sw");
 
-    self.clients.claim();
-});
 
-self.addEventListener('push', dog => {
-    const podaci = dog.data.json();
-        self.registration.showNotification(podaci.title, {
-            body: podaci.body
-        })
 });
-
 
 self.addEventListener('fetch', async dog => {
     const zahtev = dog.request;
     const url = new URL(zahtev.url);
 
-    if (url.origin === location.origin) {
-        dog.respondWith(vratiIzKesMemorije(zahtev));
-    } else {
-        dog.respondWith(vratiSaMrezeIliIzKesMemorije
-            (zahtev));
-    }
+
+    
+
 
 });
 
@@ -62,7 +51,7 @@ async function vratiSaMrezeIliIzKesMemorije(zahtev) {
         return najnovijiPodaci;
     } catch (error) {
         const kesiraniPodaci = await kes.match(zahtev);
-        return kesiraniPodaci
+        return kesiraniPodaci;
     }
 
 }
